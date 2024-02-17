f14ee321f569a86b8a397da81fac7b327072368b
Monzer-OM <96489338+Monzer-OM@users.noreply.github.com>
2024-02-17 23:01:54 +0300

    add items and logo to web

--- script.js
+++ script.js
@@ -27,6 +27,105 @@ const dishes = [
     image: "images/FIG TOAST.jpeg",
     description: "Tender chicken simmered in a creamy red curry with vegetables and fragrant spices.",
   },
+  {
+    name: "GOLDEN DELIGHT",
+    price: "43",
+    image: "images/GOLDEN DELIGHT.jpg",
+    description: "FRESH HOMEMADE CRISPY BRIOCHE / VANILA ICE CREAM / CHOCOLATE",
+  },
+  {
+    name: "SIMIT MUHAMMARA",
+    price: '38',
+    image: 'images/SIMIT MUHAMMARA.jpg',
+    description: 'EGG / MUHAMMARA / TURKISH LABNEH / CHILI BUTTER / SIMIT BREAD',
+  },
+  {
+    name: 'LIME AND CORN SALAD',
+    price:'34',
+    image: 'images/LIME AND CORN SALAD.jpg',
+    description : "BABY SPINACH / JARJEER ROCKET LEAF / FETA CHEESE / CHERRY TOMATO / GRILLED CORN / LIME / BLACK OLIVE ",
+  },
+  {
+    name: 'CROQUE MADAME',
+    price: "43",
+    image: "images/CROQUE MADAME.jpg",
+    description: "SOURDOUGH BREAD / BECHAMEL SAUCE / GRUYERE CHEESE / POUCHED EGG / SMOKED TURKEY"
+  },
+  {
+    name: 'RITUAL CAESAR SALAD',
+    price: "32",
+    image: 'images/RITUAL CAESAR SALAD.jpg',
+    description: "ROMAINE LETTUCE / HOME MADE RITUAL SAUCE / SMOKED BEEF BACON / POACHED EGG / PARMESAN CHEESE / GARLIC CROUTONS"
+  },
+  {
+    name: "BURATA SALAD",
+    price: "47",
+    image: 'images/BURATA SALAD.jpg',
+    description: 'BURATA CHEESE / DATES / BABY SPINACH SALAD',
+  },
+  {
+    name: "AVOCADO POACHED EGG",
+    price: "39",
+    image: "images/AVOCADO POACHED EGG.jpg",
+    description: "HOMEMADE SOUR DOUGH AVOCADO WITH CHOICE OF EGG", 
+  }, 
+  {
+    name: "PEANUT BUTTER TART",
+    price: "36",
+    image: "images/PEANUT BUTTER TART.jpg",
+    description: "CHOCOLATE TART BASE / TART GARNISH",
+  },
+
+  {
+    name: "PORTOBELLO BENEDICT",
+    price: "43",
+    image: "images/PORTOBELLO BENEDICT.jpg",
+    description: "PLAIN CROISSANT / TRUFFLE BECHAMEL / PORTOBELLO MUSHROOM / HOLLANDAISE SAUCE / EGG / PAPRIKA",
+  },
+
+  {
+    name: "TURKISH EGG",
+    price: '38',
+    image: 'images/TURKISH EGG.jpg',
+    description: "FRESH LABNAH / YOGURT / PARSLEY / MINT LEAF / BLACK PEPPER",
+  },
+  {
+    name: "MUSHROOM TOAST",
+    price: "34",
+    image: "images/MUSHROOM TOAST.jpg",
+    description: "HOMEMADE SOUR DOUGH / PHILADELPHIA CHEESE / TRIO MUSHROOM",
+  },
+  {
+    name: "MOZZARELLA PESTO",
+    price: "34",
+    image: "images/MOZZARELLA PESTO.jpg",
+    description: "SOUR DOUGH BREAD / PESTO / CHERRY TOMATO / BUFFALO MOZZARELLA CHEESE"
+  },
+  {
+    name: "AVOCADO TOAST",
+    price: "36",
+    image: "images/AVOCADO TOAST.jpg",
+    description: "HOMEMADE SOUR DOUGH / FETA CHEESE / AVOCADO / BLACK SEED / CHILI FLAKE / LEMON"
+  },
+  {
+    name: "AVOCADO SCRAMBLE EGG",
+    price: "39",
+    image: "images/AVOCADO SCRAMBLE EGG.jpg",
+    description:"HOMEMADE SOUR DOUGH AVOCADO WITH CHOICE OF EGG",
+  },
+  {
+    name: "OMELETTE",
+    price: "36",
+    image: "images/OMELETTE.jpg",
+    description: "STUFFED: CHEESE.....CHOOSE: TOMATO / MUSHROOM / SPINACH / MIXED SALAD"
+  },
+  {
+    name: "FLUFFY CHEESE CAKE",
+    price: "36",
+    image: "images/FLUFFY CHEESE CAKE.jpg",
+    description: 'CREAM CHEESE / LEMON EXTRACT / DIGESTIVE BISCUIT / BLUEBERRY SAUCE'
+  }
+
   // Add remaining dishes here with their details
 ];
 
@@ -109,8 +208,3 @@ sendOrderButton.addEventListener('click', () => {
   window.open(url, '_blank');
 });
 
-
-
-
-
-// Add logic for order list and further functionality in this script
